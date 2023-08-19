9eafa89cf875f9925f95e6a068c65425aa9b992b
Arthur Fernandez <arthur@snooget.com>
2023-08-19 06:59:21 -0700

    setup environment, clerk, & modals

---
+++ providers/modal-provider.tsx
@@ -0,0 +1,24 @@
+"use client";
+// Global imports below
+import { StoreModal } from "@/components/modals/store-modal";
+
+// Local imports below:
+import { useEffect, useState } from "react";
+
+export const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <>
+      <StoreModal />
+    </>
+  );
+};
