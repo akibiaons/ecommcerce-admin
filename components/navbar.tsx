da7a2b15b97a44ba8d6da987730f18b8b4d4d5e2
Arthur Fernandez <arthur@snooget.com>
2023-08-20 09:26:15 -0700

    Added navbar, main navbar functions, and params

---
+++ components/navbar.tsx
@@ -0,0 +1,23 @@
+// This file is not in ui, because it is not a reusable component...
+
+import { UserButton } from "@clerk/nextjs";
+
+import { MainNav } from "@/components/main-nav";
+
+const Navbar = () => {
+  return (
+    <div className="border-b">
+      <div className="flex h-16 items-center px-4">
+        <div>This will be a store switcher</div>
+        <div>
+          <MainNav className="mx-6" />
+        </div>
+        <div className="ml-auto flex items-center space-x-4">
+          <UserButton afterSignOutUrl="/" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
