9d3d9d47e56ad22f2458c8d3bfdfc581f0b087e9
Arthur Fernandez <arthur@snooget.com>
2023-08-20 11:50:31 -0700

    Added store toggle component, and redirect to nav

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,15 +1,29 @@
 // This file is not in ui, because it is not a reusable component...
 
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 import { MainNav } from "@/components/main-nav";
 import StoreSwitcher from "@/components/store-switcher";
+import prismadb from "@/lib/prismadb";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const stores = await prismadb.store.findMany({
+    where: {
+      userId,
+    },
+  });
+
   return (
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
-        <StoreSwitcher />
+        <StoreSwitcher items={stores} />
         <div>
           <MainNav className="mx-6" />
         </div>
