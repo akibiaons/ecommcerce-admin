81cb33bfbf9e7cec623034da6ce0ff2b59de2522
Arthur Fernandez <arthur@snooget.com>
2023-08-20 09:43:20 -0700

    new ui shad cn import, new store switcher component

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,5 +1,6 @@
 "use client";
-
+//Seperate
+import Link from "next/link";
 import { useParams, usePathname } from "next/navigation";
 // Seperate
 import { cn } from "@/lib/utils";
@@ -11,10 +12,29 @@ export function MainNav({
   const pathname = usePathname();
   const params = useParams();
   // Seperate
-  const routes = [];
+  const routes = [
+    {
+      href: `/${params.storeId}/settings`,
+      label: "Settings",
+      active: pathname === `/${params.storeId}/settings`,
+    },
+  ];
   return (
-    <nav
-      className={cn("flex items-center space-x-4 lg:space-x-6", className)}
-    ></nav>
+    <nav className={cn("flex items-center space-x-4 lg:space-x-6", className)}>
+      {routes.map((route) => (
+        <Link
+          key={route.href}
+          href={route.href}
+          className={cn(
+            "text-sm font-medium transition-colors hover:text-primary",
+            route.active
+              ? "text-black dark:text-white"
+              : "text-muted-foreground"
+          )}
+        >
+          {route.label}
+        </Link>
+      ))}
+    </nav>
   );
 }
