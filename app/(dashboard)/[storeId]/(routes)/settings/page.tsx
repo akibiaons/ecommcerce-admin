3387fb2c5071ce4532208578c2a003ed00e3da11
Arthur Fernandez <arthur@snooget.com>
2023-08-20 17:50:38 -0700

    fixed new site feature, fixed bug for changing site name

--- app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -13,11 +13,9 @@ interface SettingsPageProps {
 
 const SettingsPage: React.FC<SettingsPageProps> = async ({ params }) => {
   const { userId } = auth();
-  //
   if (!userId) {
     redirect("/sign-in");
   }
-  //
   const store = await prismadb.store.findFirst({
     where: {
       id: params.storeId,
@@ -28,7 +26,6 @@ const SettingsPage: React.FC<SettingsPageProps> = async ({ params }) => {
   if (!store) {
     redirect("/");
   }
-  //
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
--- app/api/stores/route.ts
+++ app/api/stores/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import { auth } from "@clerk/nextjs";
+
 import prismadb from "@/lib/prismadb";
 
 export async function POST(req: Request) {
@@ -10,7 +11,7 @@ export async function POST(req: Request) {
     const { name } = body;
 
     if (!userId) {
-      return new NextResponse("Unauthorized", { status: 401 });
+      return new NextResponse("Unauthorized", { status: 403 });
     }
 
     if (!name) {
@@ -27,6 +28,6 @@ export async function POST(req: Request) {
     return NextResponse.json(store);
   } catch (error) {
     console.log("[STORES_POST]", error);
-    return new NextResponse("Internal Error", { status: 500 });
+    return new NextResponse("Internal error", { status: 500 });
   }
 }
