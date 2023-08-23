a31abb0961308121597f4828b917a0abbdcb1ba6
Arthur Fernandez <arthur@snooget.com>
2023-08-23 08:31:25 -0700

    Added pt1 of GET function to billboard route

--- app/api/[storeId]/billboards/[billboardId]/route.ts
+++ app/api/[storeId]/billboards/[billboardId]/route.ts
@@ -3,6 +3,47 @@ import { NextResponse } from "next/server";
 
 import prismadb from "@/lib/prismadb";
 
+export async function GET(
+  req: Request,
+  { params }: { params: { storeId: string; billboardId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 401 });
+    }
+
+    if (!params.billboardId) {
+      return new NextResponse("Store id is required", { status: 400 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorized", { status: 403 });
+    }
+
+    const billboard = await prismadb.store.deleteMany({
+      where: {
+        id: params.billboardId,
+      },
+    });
+
+    return NextResponse.json(billboard);
+  } catch (error) {
+    console.log("[BILLBOARD_DELETE]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+// Below is the PATCH Function
+//======================================================================================================================================================
 export async function PATCH(
   req: Request,
   { params }: { params: { storeId: string; billboardId: string } }
@@ -56,7 +97,8 @@ export async function PATCH(
     return new NextResponse("Internal error", { status: 500 });
   }
 }
-
+// Below is the DELETE Function
+//======================================================================================================================================================
 export async function DELETE(
   req: Request,
   { params }: { params: { storeId: string; billboardId: string } }
