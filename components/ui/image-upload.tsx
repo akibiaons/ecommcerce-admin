d17c6d715f226a4bc4c67ae65d9fc3b5007afd1f
Arthur Fernandez <arthur@snooget.com>
2023-08-22 18:49:53 -0700

    self closing tag

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -68,7 +68,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
               variant="secondary"
               onClick={onClick}
             >
-              <ImagePlus className="h-4 w-4 mr-2"></ImagePlus>
+              <ImagePlus className="h-4 w-4 mr-2" />
               Upload an image
             </Button>
           );
