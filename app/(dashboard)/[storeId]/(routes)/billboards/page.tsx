836ec2fa869ff567b7a737d953e91ebcc7e73f2b
Arthur Fernandez <arthur@snooget.com>
2023-08-23 20:14:16 -0700

    added new table deps, billboards page memory too

--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,10 +1,21 @@
+import prismadb from "@/lib/prismadb";
+// Seperate
 import { BillboardClient } from "./components/client";
 
-const BillboardsPage = () => {
+const BillboardsPage = async ({ params }: { params: { storeId: string } }) => {
+  const Billboards = await prismadb.billboard.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient />
+        <BillboardClient data={Billboards} />
       </div>
     </div>
   );
