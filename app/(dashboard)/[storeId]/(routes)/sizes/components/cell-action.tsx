d21f9a8270bb6e4a3a68c207e2c9deb57a74ebff
Arthur Fernandez <arthur@snooget.com>
2023-08-26 09:38:13 -0700

    Added routes for actions on sizes

--- app/(dashboard)/[storeId]/(routes)/sizes/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/sizes/components/cell-action.tsx
@@ -18,11 +18,11 @@ import {
 } from "@/components/ui/dropdown-menu";
 // Seperate == ===  ==  =
 
-import { BillboardColumn } from "./columns";
+import { SizeColumn } from "./columns";
 import { AlertModal } from "@/components/modals/alert-modal";
 
 interface CellActionProps {
-  data: BillboardColumn;
+  data: SizeColumn;
 }
 
 export const CellAction: React.FC<CellActionProps> = ({ data }) => {
@@ -34,17 +34,17 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
 
   const onCopy = (id: string) => {
     navigator.clipboard.writeText(id);
-    toast.success("Billboard Id copied to clipboard");
+    toast.success("Size Id copied to clipboard");
   };
 
   const onDelete = async () => {
     try {
       setLoading(true);
-      await axios.delete(`/api/${params.storeId}/billboards/${data.id}`);
+      await axios.delete(`/api/${params.storeId}/sizes/${data.id}`);
       router.refresh();
-      toast.success("Billboard Deleted");
+      toast.success("Size Deleted");
     } catch (error) {
-      toast.error("Make sure you removed all categories first.");
+      toast.error("Make sure you removed all products using this size first.");
     } finally {
       setLoading(false);
       setOpen(false);
@@ -74,9 +74,7 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
             Copy Id
           </DropdownMenuItem>
           <DropdownMenuItem
-            onClick={() =>
-              router.push(`/${params.storeId}/billboards/${data.id}`)
-            }
+            onClick={() => router.push(`/${params.storeId}/sizes/${data.id}`)}
           >
             <Edit className="mr-2 h-4 w-4" />
             Update
